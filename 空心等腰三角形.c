50b7880d385656130de32eba0e7d7f9c657ba852
Amstra <34560790+DTSSR@users.noreply.github.com>
2019-04-19 15:45:22 +0800

    Add files via upload

---
+++ 空心等腰三角形.c
@@ -0,0 +1,29 @@
+#include<stdio.h>
+int main(){
+	int a,i,j;
+	printf("输入三角形行数:");
+	scanf("%d",&a);
+	for(i=0;i<=a-2;i++){
+		printf(" ");
+	}
+	printf("*\n");
+	for(i=1;i<=a-2;i++){
+		for(j=1;j<=i+a;j++){
+			if(j==a-i){
+				printf("/");
+			}
+			else if(j==a+i){
+				printf("\\");				
+			}
+			else{
+				printf(" ");
+			}
+		}
+		printf("\n");
+	}
+	for(i=1; i<=2*a-1; i++){
+	printf("-");
+	}
+	printf("\n王少奇 建环-2 201701041526\n"); 
+}
+
